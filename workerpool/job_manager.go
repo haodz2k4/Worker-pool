e43680a97d811a7eb8b2568b14dc89da54cb27a6
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-08-20 16:01:58 +0700

    job manager

--- workerpool/job_manager.go
+++ workerpool/job_manager.go
@@ -1,1 +1,22 @@
 package workerpool
+
+import "sync"
+
+type JobStatus int
+
+const (
+	Pending JobStatus = iota
+	Processing
+	Completed
+)
+
+type JobManager struct {
+	jobStatus map[*Job]JobStatus
+	mu        sync.Mutex
+}
+
+func NewJobManager() *JobManager {
+	return &JobManager{
+		jobStatus: make(map[*Job]JobStatus),
+	}
+}
